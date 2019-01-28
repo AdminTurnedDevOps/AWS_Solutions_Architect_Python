83edc129b49dd4de0a6c599650fe49ad87909bd2
pafable <philafable@gmail.com>
2019-01-27 22:32:20 -0500

    vpc test 2

--- create_vpc.py
+++ create_vpc.py
@@ -1,11 +1,14 @@
 import boto3
 import sys
 
-ec2 = boto3.resource('ec2')
-vpc = ec2.create_vpc(
-                     CidrBlock='10.100.0.0/16',
-                     AmazonProvidedIpv6CidrBlock=False,
-                     InstanceTenancy='default'
-)
+def create_VPC():
 
-print(vpc)
+    ec2 = boto3.resource('ec2')
+    vpc = ec2.create_vpc(
+                         CidrBlock='10.100.0.0/16',
+                         AmazonProvidedIpv6CidrBlock=False,
+                         InstanceTenancy='default'
+                        )
+    return vpc
+
+print(create_VPC())
