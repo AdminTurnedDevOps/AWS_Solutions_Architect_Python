7e90e451b91b79ca1fa2efcb9192edf92bac65a9
pafable <philafable@gmail.com>
2019-01-28 21:34:44 -0500

    added subnet and igw creation

--- create_vpc.py
+++ create_vpc.py
@@ -1,18 +1,50 @@
 import boto3
 import sys
 
-def create_VPC(cidr, amzIP6, tenancy):
+ec2 = boto3.resource('ec2')
 
-    ec2 = boto3.resource('ec2')
+def create_VPC(cidr, tenancy, name):
+    #Create vpc
     vpc = ec2.create_vpc(
                          CidrBlock=cidr,
-                         AmazonProvidedIpv6CidrBlock=bool(amzIP6),
+                         AmazonProvidedIpv6CidrBlock=False,
                          InstanceTenancy=tenancy
                         )
+    vpc.create_tags(
+                    Tags=[{
+                        "Key": "Name",
+                        "Value": name
+                    }])
+
+    #Create subnet
+    subnet = ec2.create_subnet(
+                               CidrBlock='10.100.1.0/24',
+                               VpcId=vpc.id
+                              )
+    subnet.create_tags(
+        Tags=[{
+            "Key": "Name",
+            "Value": name
+        }])
+
+    #Create internet gateway
+    igw = ec2.create_internet_gateway()
+    igw.attach_to_vpc(
+                      VpcId=vpc.id
+                     )
+    igw.create_tags(
+        Tags=[{
+            "Key": "Name",
+            "Value": name
+        }])
+
+    return subnet
     return vpc
+    return igw
+
 
 cidr = sys.argv[1]
-amzIP6 = sys.argv[2]
-tenancy = sys.argv[3]
+tenancy = sys.argv[2]
+name = sys.argv[3]
 
-print(create_VPC(cidr, amzIP6, tenancy))
+print(create_VPC(cidr, tenancy, name))
