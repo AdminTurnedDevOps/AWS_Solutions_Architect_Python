a54f7393ade5614d4abc873cdadd802d5f4d9c0c
AdminTurnedDevOps <mlevan1992@gmail.com>
2019-02-13 11:11:10 -0500

    first iteration of RDS script to create a MySQL instance

---
+++ new_rdsmysql.py
@@ -0,0 +1,46 @@
+import boto3
+import sys
+import time
+import logging
+
+def new_rdsmysql(dbname, instanceID, storage, dbInstancetype, dbusername, masterPass):
+    if len(masterPass) < 10:
+        logging.warning('Password is not at least 10 characters. Please try again')
+        time.sleep(5)
+        exit
+    else:
+        None
+    
+    rds_instance = boto3.client('rds')
+    create_instance = rds_instance.create_db_instance(
+        DBName = dbname,
+        DBInstanceIdentifier = instanceID,
+        AllocatedStorage = int(storage),
+        DBInstanceClass = dbInstancetype,
+        Engine = 'mysql',
+        MasterUsername = dbusername,
+        MasterUserPassword = str(masterPass),
+        MultiAZ = True,
+        EngineVersion = '5.7.23',
+        AutoMinorVersionUpgrade = False,
+        LicenseModel = 'general-public-license',
+        PubliclyAccessible = False,
+
+        Tags = [
+            {
+            'Key': 'Name',
+            'Value' : dbname
+            }
+        ]
+    )
+
+    print(create_instance)
+
+dbname = sys.argv[1]
+instanceID = sys.argv[2]
+storage = sys.argv[3]
+dbInstancetype = sys.argv[4]
+dbusername = sys.argv[5]
+masterPass = sys.argv[6]
+
+new_rdsmysql(dbname, instanceID, storage, dbInstancetype, dbusername, masterPass)
